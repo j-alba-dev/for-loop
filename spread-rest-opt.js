7870884bdbc34a9f445cce2407c54baf888f5e69
datalyst1127 <96359837+datalyst1127@users.noreply.github.com>
2023-04-27 19:09:35 -0700

    completed spread and rest

--- spread-rest-opt.js
+++ spread-rest-opt.js
@@ -1,19 +1,20 @@
 // Write a function that takes two arrays and returns a new array that contains all the elements of both arrays, using the spread operator.
 
-function concatArrays(arr1, arr2){
-    // TODO, write your code here
+function concatArrays(arr1, arr2) {
+  // TODO, write your code here
+  return [...arr1, ...arr2];
 }
 
 console.log(concatArrays([1, 2, 3], [4, 5, 6])); // Output: [1, 2, 3, 4, 5, 6]
-console.log(concatArrays(['apple', 'banana'], ['cherry', 'dragonfruit'])); // Output: ['apple', 'banana', 'cherry', 'dragonfruit']
-
+console.log(concatArrays(["apple", "banana"], ["cherry", "dragonfruit"])); // Output: ['apple', 'banana', 'cherry', 'dragonfruit']
 
 // Write a function that takes an array and returns a new array with the first and last elements removed, using the rest operator.
 
-function removeFirstAndLast(){
-
+function removeFirstAndLast(arr) {
+  const [first, ...rest] = arr;
+  rest.pop();
+  return rest;
 }
 
 console.log(removeFirstAndLast([1, 2, 3, 4])); // Output: [2, 3]
-console.log(removeFirstAndLast(['apple', 'banana', 'cherry', 'dragonfruit'])); // Output: ['banana', 'cherry']
-
+console.log(removeFirstAndLast(["apple", "banana", "cherry", "dragonfruit"])); // Output: ['banana', 'cherry']
